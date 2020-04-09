365eaac96bca0932dc24091294d470d03e06918d
Littlefieldm <54365866+Littlefieldm@users.noreply.github.com>
2020-04-08 21:21:04 -0500

    Fixed slideshow JS

--- js/app.js
+++ js/app.js
@@ -50,9 +50,11 @@ slideShow();
 function slideShow(){
 	var i;
 	var setslides = document.getElementByClassName("myPics");
-	for( i=0; i < setSlides.length; i++)
-	{
-		setSlides[i].style.display = "block";
-		setTimeout(showSlides, 2000);
-	}
+	for (i = 0; i < setslides.length; i++) {
+    setslides[i].style.display = "none";
+    }
+    slideNum++;
+    if (slideNum > setslides.length) {slideNum = 1}
+    setslides[slideNum-1].style.display = "block";
+    setTimeout(slideShow, 2000); // Change image every 2 seconds
 }
