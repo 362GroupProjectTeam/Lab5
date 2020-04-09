0c8cf9c4af1261b0d2dd5b4d7d6330af9ce7abfe
Littlefieldm <54365866+Littlefieldm@users.noreply.github.com>
2020-04-08 22:11:40 -0500

    Add Slideshow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+//Set slideshow var
+var slideNum = 0;
+
+//Call Function
+slideShow();
+
+//SlideShow Function
+function slideShow() {
+  var i; //Counter
+  var pic = document.getElementsByClassName("myPic");  //Grab the pictures
+  for (i = 0; i < pic.length; i++) {
+    pic[i].style.display = "none";
+  }
+  slideNum++;  //increment picture shown
+  if (slideNum > pic.length) {slideNum = 1}
+  pic[slideNum-1].style.display = "block";
+  
+  //Time between pictures
+  setTimeout(slideShow, 5000);
+}
+
+
+
+
+
 //Form Elements
 var fName;
 var lName;
